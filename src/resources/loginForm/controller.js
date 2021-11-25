0afb6bd5b920d5fe4dcfa3e7550e33c74320d996
Lana Ferrari <ferrarirangel@gmail.com>
2021-11-25 17:49:37 +0000

    Adding bcrypt and token

--- src/resources/loginForm/controller.js
+++ src/resources/loginForm/controller.js
@@ -1,4 +1,6 @@
 const prisma = require("../../utils/dbClient");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 async function checkUser(req, res) {
   const { email, password: passwordFromRequest } = req.body;
@@ -17,8 +19,28 @@ async function checkUser(req, res) {
 
     const { password: passwordFromData } = user;
 
-    if (passwordFromData === passwordFromRequest) {
-      res.status(200).json({ user });
+    const matchingPassword = await bcrypt.compare(
+      passwordFromRequest,
+      passwordFromData
+    );
+
+    // createToken
+    // - input: an object that represents a user
+    // - output: a string that represents a json web token
+
+    const createToken = (user) => {
+      const token = jwt.sign({ ...user }, process.env.JWT_SECRET, {
+        expiresIn: "8h",
+      });
+
+      return token;
+    };
+
+    const token = createToken(result);
+    console.log("Token", token);
+
+    if (matchingPassword) {
+      res.status(200).json({ token });
     } else {
       res.status(401).json({ message: "Not authorized" });
     }
--- src/resources/users/controller.js
+++ src/resources/users/controller.js
@@ -1,16 +1,37 @@
 const prisma = require("../../utils/dbClient");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { token } = require("morgan");
 
 const createUser = async (req, res) => {
   const { email, password } = req.body;
+
+  const incriptedPassword = await bcrypt.hash(password, 8);
 
   try {
     const result = await prisma.user.create({
       data: {
         email,
-        password,
+        password: incriptedPassword,
       },
     });
-    res.status(201).json(result);
+
+    // createToken
+    // - input: an object that represents a user
+    // - output: a string that represents a json web token
+
+    const createToken = (result) => {
+      const token = jwt.sign({ ...result }, process.env.JWT_SECRET, {
+        expiresIn: "8h",
+      });
+
+      return token;
+    };
+
+    const token = createToken(result);
+    console.log("Token", token);
+
+    res.status(201).json(token);
   } catch (error) {
     console.error({ error: error.message });
     res.status(500).json({ error: error.message });
