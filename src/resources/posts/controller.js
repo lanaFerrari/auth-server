28e444a171c4d964b649fd009bb6c1e80ec7e8d5
Lana Ferrari <ferrarirangel@gmail.com>
2021-11-26 15:00:27 +0000

    Updating getAllPost with jwt

--- src/resources/posts/controller.js
+++ src/resources/posts/controller.js
@@ -2,13 +2,32 @@ const dbClient = require("../../utils/dbClient");
 const prisma = dbClient;
 
 const getAllPosts = async (req, res) => {
-  try {
-    const result = await prisma.post.findMany();
-    res.json({ result });
-  } catch (error) {
-    console.error({ error: error.message });
-    res.status(500).json({ error: error.message });
-  }
+  console.log({ authorization: req.headers.authorization });
+
+  const token = req.headers.authorization;
+
+  jwt.verify(token, process.env.JWT_SECRET, async (err, payload) => {
+    if (err) {
+      throw Error("Not Authorized");
+    }
+
+    console.log({ payload });
+
+    try {
+      const posts = await prisma.post.findMany({
+        where: {
+          userId: payload.id,
+        },
+        include: {
+          user: true,
+        },
+      });
+
+      res.json({ posts });
+    } catch (error) {
+      console.error({ error });
+    }
+  });
 };
 
 module.exports = { getAllPosts };
