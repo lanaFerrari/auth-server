e58088159a7ab95657451c57d51ecab9d5e2772e
Lana Ferrari <ferrarirangel@gmail.com>
2021-11-26 10:08:16 +0000

    Login controller done

--- src/resources/loginForm/controller.js
+++ src/resources/loginForm/controller.js
@@ -1,45 +1,42 @@
 const prisma = require("../../utils/dbClient");
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+const { createToken } = require("../../utils/authentication");
 
 async function checkUser(req, res) {
   const { email, password: passwordFromRequest } = req.body;
   //Another way of saying: const passwordFromRequest = req.body.password
 
+  if (!email || !passwordFromRequest) {
+    res.status(400).json({ error: "Email or Password missing" });
+  }
+
   try {
-    const user = await prisma.user.findUnique({
+    const foundUser = await prisma.user.findUnique({
       where: {
         email,
       },
     });
 
-    if (!user) {
+    if (!foundUser) {
       res.status(401).json({ message: "Not Authorized" });
     }
 
-    const { password: passwordFromData } = user;
+    const { password: passwordFromData } = foundUser;
 
     const matchingPassword = await bcrypt.compare(
       passwordFromRequest,
       passwordFromData
     );
 
-    // createToken
-    // - input: an object that represents a user
-    // - output: a string that represents a json web token
-
-    const createToken = (user) => {
-      const token = jwt.sign({ ...user }, process.env.JWT_SECRET, {
-        expiresIn: "8h",
-      });
-
-      return token;
-    };
-
-    const token = createToken(result);
-    console.log("Token", token);
-
     if (matchingPassword) {
+      const userToToken = {
+        ...foundUser,
+      };
+
+      delete userToToken.password;
+
+      const token = createToken(userToToken);
+
       res.status(200).json({ token });
     } else {
       res.status(401).json({ message: "Not authorized" });
